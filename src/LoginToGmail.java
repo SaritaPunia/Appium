de18922f6ead4faaddbe69bf87445f4de3dd0ee2
iOSQA <iosqa@iOSQAs-MacBook-Pro.local>
2017-10-23 21:58:33 +0530

    Testing

--- src/LoginToGmail.java
+++ src/LoginToGmail.java
@@ -1,4 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 
 public class LoginToGmail {
+	
+	public static void main(String args[]){
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("www.gmail.com");
+		driver.findElement(By.id("id")).sendKeys("userName");
+		driver.findElement(By.id("id")).sendKeys("password");
+		driver.findElement(By.id("Login")).click();
+		Assert.assertEquals("GMAil", driver.findElement(By.id("id")).getText());
+		driver.findElement(By.id("Compose")).click();
+		driver.findElement(By.xpath("\\")).sendKeys("EmailAddress");
+		driver.findElement(By.name("Subject")).sendKeys("SubjectOfMail");
+		
+	}
 
 }
